d7bbeebbbfa64ba6984b7242eac2b6a34e2588c2
Stefan <samyotte@phirelight.com>
2014-02-03 15:11:42 -0500

    cleaning up www folder

--- www/assets/modals/ioc.upload.php
+++
@@ -1,7 +0,0 @@
-<div style="height:100px; overflow:hidden">
-<form enctype="multipart/form-data" action="inc/upload-handler.php" method="POST">
-<input style="width:100%" type="hidden" name="MAX_FILE_SIZE" value="512000" />
- Upload file: <input name="userfile" type="file" />
-<input style="float:right" class="btn red" type="submit" value="Upload" />
-</form>
-</div>
