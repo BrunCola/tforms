d29f0484f0bf874feb0ca3406c4dca013b285b8d
Stefan <samyotte@phirelight.com>
2014-02-10 14:02:46 -0500

    index.php cleaning

--- report.php
+++ report.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
-		<meta content="" name="rapidPHIRE">
-		<meta content="" name="Phirelight Security Solutions">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+		<meta content="" name="rapidPHIRE"/>
+		<meta content="" name="Phirelight Security Solutions"/>
 		<title>rapidPHIRE | fight fire with PHIRE |</title>
-		<link rel="shortcut icon" href="assets/favicon.ico">
-		<link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-		<link href="assets/lib/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
-		<link href="assets/lib/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/style-metro.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/style.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/dc.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/style-responsive.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/themes/default.css" rel="stylesheet" type="text/css">
-		<link href="assets/lib/uniform/css/uniform.default.css" rel="stylesheet" type="text/css">
-		<link href="assets/lib/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/jquery.dataTables.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/jdashboard.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/ColVis.css" rel="stylesheet" type="text/css">
-		<!-- <link href="assets/css/flags16.css" rel="stylesheet" type="text/css"> currently not being used -->
-		<link href="assets/css/flags32.css" rel="stylesheet" type="text/css">
+		<link rel="shortcut icon" href="assets/favicon.ico"/>
+		<link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/lib/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/lib/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/style-metro.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/dc.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/style-responsive.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/themes/default.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/lib/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/lib/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/jquery.dataTables.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/jdashboard.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/ColVis.css" rel="stylesheet" type="text/css"/>
+		<!-- <link href="assets/css/flags16.css" rel="stylesheet" type="text/css"/> currently not being used -->
+		<link href="assets/css/flags32.css" rel="stylesheet" type="text/css"/>
 		<!-- <link rel="stylesheet" type="text/css" href="assets/css/parallel.css"> -->
-		<link href="assets/css/TableTools.css" rel="stylesheet" type="text/css">
-		<link href="assets/css/colorbox.css" rel="stylesheet" type="text/css">
+		<link href="assets/css/TableTools.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/colorbox.css" rel="stylesheet" type="text/css"/>
 		<script type="text/javascript" src="assets/lib/jquery-1.10.2.min.js"></script>
 		<script type="text/javascript" src="assets/lib/noty/jquery.noty.js"></script>
 		<script type="text/javascript" src="assets/lib/noty/layouts/top.js"></script>
@@ -59,24 +59,24 @@
 		<!-- <script type="text/javascript" src="inc/email.js"></script> -->
 		<script type="text/javascript" src="inc/template.js"></script>
 		<script type="text/javascript" src="inc/glossary.js"></script>
+		<style>
+			@media print {
+				#table1 {
+					page-break-before: always !important;
+				}
+				table tr td, table.print-friendly tr th {
+					page-break-inside: avoid;
+				}
+			}
+		</style>
 	</head>
-	<style>
-		@media print {
-			#table1 {
-				page-break-before: always !important;
-			}
-			table tr td, table.print-friendly tr th {
-				page-break-inside: avoid;
-			}
-		}
-	</style>
 	<body style="background-color:#ebeaeb;width:1056px !important;" class="page-header">  		
 		<!-- HEADER -->
 		<div class="header navbar navbar-inverse navbar-top">
 			<div class="navbar-inner">
 				<div style="background-color: #000;" class="container-report-fluid">
 					<a href="#">
-						<img class="logo" src="assets/img/logo.png">
+						<img class="logo" src="assets/img/logo.png" alt=""/>
 					</a>
 					<div style="padding-top:10px;position:relative;color:#fff;">
 						<span id="graph_title" style="color:#fff !important;margin-left:7px;font-size:14px;"></span>
