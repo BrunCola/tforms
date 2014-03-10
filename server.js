0750a687c2ab6bf3218f6fbdb7943abe30f5c707
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-03-10 13:17:08 -0400

    Upgrades to ssl redirecting

--- server.js
+++ server.js
@@ -53,13 +53,15 @@ require('./config/passport')(passport, connection);
 var options = {
     key: fs.readFileSync('./ssl/server.key'),
     cert: fs.readFileSync('./ssl/server.crt'),
+    requestCert: true
 };
 
 var app = express()
+    , httpapp = express()
+    , http = require('http').createServer(httpapp)
     , https = require('https')
     , server = https.createServer(options, app)
     , io = require('socket.io').listen(server);
-
 
 // Express settings
 require('./config/express')(app, passport, connection);
@@ -87,9 +89,14 @@ walk(routes_path);
 
 
 // Start the app by listening on <port>
-var port = process.env.PORT || config.port;
-server.listen(port);
-console.log('rapidPHIRE app started on port ' + port);
+var SSLport = process.env.sslPORT || config.SSLport;
+var HTTPport = process.env.httpPORT || config.HTTPport;
+httpapp.get('*',function(req, res){
+    res.redirect('https://localhost:'+SSLport+req.url)
+})
+server.listen(SSLport),
+http.listen(HTTPport);
+console.log('rapidPHIRE app started on port ' + SSLport);
 
 // Initializing logger
 // logger.init(app, passport, mongoose);
