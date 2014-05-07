e6e903447fc7da2c0047cbe55b1c03107d98c5f1
rapidphire2 <rapidphire2@Rapidphires-Mac-mini.local>
2014-05-07 09:34:26 -0400

    first commit of branch

--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.iochits', 'ui.bootstrap']);
-
-angular.module('mean.system', ['ui.bootstrap']);
-angular.module('mean.articles', []);
-angular.module('mean.iochits', ['ui.bootstrap']);
