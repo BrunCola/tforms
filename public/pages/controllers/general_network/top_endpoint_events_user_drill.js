72a35b76428ce0d14ace392bc33abedad7f6a799
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-05-09 13:51:29 -0400

    fixed endpoint drill link

--- public/pages/controllers/general_network/top_endpoint_events_user_drill.js
+++ public/pages/controllers/general_network/top_endpoint_events_user_drill.js
@@ -4,9 +4,9 @@ angular.module('mean.pages').controller('topEndpointEventsUserDrillController', 
 	$scope.global = Global;
 	var query;
 	if ($location.$$search.start && $location.$$search.end) {
-		query = '/general_network/top_endpoint_events_user_drill?start='+$location.$$search.start+'&end='+$location.$$search.end+'&alert_info='+$location.$$search.alert_info+'&usr_src='+$location.$$search.usr_src;
+		query = '/general_network/top_endpoint_events_user_drill?start='+$location.$$search.start+'&end='+$location.$$search.end+'&alert_info='+$location.$$search.alert_info+'&src_user='+$location.$$search.src_user;
 	} else {
-		query = '/general_network/top_endpoint_events_user_drill?&alert_info='+$location.$$search.alert_info+'&usr_src='+$location.$$search.usr_src;
+		query = '/general_network/top_endpoint_events_user_drill?&alert_info='+$location.$$search.alert_info+'&src_user='+$location.$$search.src_user;
 	}
 	$http({method: 'GET', url: query}).
 	//success(function(data, status, headers, config) {
