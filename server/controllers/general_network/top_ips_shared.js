78179410dd7de3e79c25dc44e5d55d35d3c8a87d
Stefan Amyotte <samyotte@phirelight.com>
2014-05-30 08:23:24 -0400

    missing space

--- server/controllers/general_network/top_ips_shared.js
+++ server/controllers/general_network/top_ips_shared.js
@@ -38,7 +38,7 @@ exports.render = function(req, res) {
 				`conn` ' +
 			'WHERE '+ 
 				'time BETWEEN '+start+' AND '+end+' ' +
-				'AND `lan_zone` = \''+req.query.lan_zone+'\''+
+				'AND `lan_zone` = \''+req.query.lan_zone+'\' '+
 				'AND `lan_ip` = \''+req.query.lan_ip+'\' ' +
 				'AND `remote_ip` = \''+req.query.remote_ip+'\' ';
 				
