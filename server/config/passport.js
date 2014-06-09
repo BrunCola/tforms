4a1132ea9aee0d88ab2895e329c150b535becd27
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-06-09 11:33:00 -0400

    MOAR auth

--- server/config/passport.js
+++ server/config/passport.js
@@ -12,20 +12,25 @@ module.exports = function(passport, connection) {
 			//to compare password that user supplies in the future
 			var hash = "SELECT * FROM user WHERE email = '"+ email +"' limit 1";
 			connection.query(hash, function (err, results) {
-				var res = results[0];
-				bcrypt.compare(password, res.password, function(err, doesMatch){
-					if (doesMatch){
-						passport.serializeUser(function(res, done) {
-							done(null, res);
-						});
-						passport.deserializeUser(function(id, done) {
-							done(null, res);
-						});
-						return done(null, res);
-					}else{
-						return done(null, false);
-					}
-				});
+				if (err) throw err;
+				if(results.length > 0){
+					var res = results[0];
+					bcrypt.compare(password, res.password, function(err, doesMatch){
+						if (doesMatch){
+							passport.serializeUser(function(res, done) {
+								done(null, res);
+							});
+							passport.deserializeUser(function(id, done) {
+								done(null, res);
+							});
+							return done(null, res);
+						}else{
+							return done(null, false);
+						}
+					});
+				} else {
+					return done(null, false);
+				}
 			});
 		}
 	));
