0fb87ee526a19dfe654f0ce5122e7cb4c887911d
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-06-09 15:22:43 -0400

    some more cleaning

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,43 +1,37 @@
 'use strict';
 
 /**
- * Module dependencies.
- */
-// var mongoose = require('mongoose'),
-//     // User = mongoose.model('User');
-
-/**
  * Auth callback
  */
 exports.authCallback = function(req, res) {
-    res.redirect('/');
+	res.redirect('/');
 };
 
 /**
  * Show login form
  */
 exports.signin = function(req, res) {
-    if(req.isAuthenticated()) {
-        return res.redirect('/');
-    }
-    res.redirect('#!/login');
+	if(req.isAuthenticated()) {
+		return res.redirect('/');
+	}
+	res.redirect('#!/login');
 };
 
 /**
  * Logout
  */
 exports.signout = function(req, res) {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 };
 
 /**
  * Session
  */
 exports.session = function(req, res) {
-    res.redirect('/');
+	res.redirect('/');
 };
 
 exports.me = function(req, res) {
-    res.jsonp(req.user || null);
+	res.jsonp(req.user || null);
 };
