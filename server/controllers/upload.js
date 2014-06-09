c8ec90fa2e17c4268978385a81fd59479f2acdc3
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-06-09 16:52:45 -0400

    multipart working

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,10 +1,6 @@
 'use strict';
 
+
 exports.render = function(req, res) {
-	console.log('got post request')
-	// if (req.url === '/upload' && req.method === 'POST') {
-	// parse a file upload
-		console.log(req.files)
-	// 	return;
-	// }
+	console.log(req.files)
 };
