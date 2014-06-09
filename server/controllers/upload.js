e7dd70a0891c89cd8349a2a37a0cc95dcd4b0a8d
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-06-09 17:08:50 -0400

    tested file upload from server side works

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,6 +1,15 @@
 'use strict';
 
+var fs = require('fs');
 
 exports.render = function(req, res) {
-	console.log(req.files)
+	for (var i in req.files) {
+		console.log(req.files[i]);
+		fs.readFile(req.files[i].path, function (err, data) {
+			var newPath = "./public/uploads/"+req.files[i].name;
+			fs.writeFile(newPath, data, function (err) {
+				res.redirect("/");
+			});
+		});
+	}
 };
