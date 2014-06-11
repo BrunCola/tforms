2d2eca2c6eaa122b9590b9dbda7b8a01787ced38
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-06-11 09:39:34 -0400

    fixed http port issue?

--- server.js
+++ server.js
@@ -4,20 +4,20 @@
  * Module dependencies.
  */
 var fs = require('fs'),
-    passport = require('passport'),
-    config = require('./server/config/config'),
+	passport = require('passport'),
+	config = require('./server/config/config'),
 	mysql = require('mysql'),
-    express = require('express');
-    // logger = require('mean-logger');
+	express = require('express');
+	// logger = require('mean-logger');
 
 
 // Initializing system variables
 var db = mysql.createPool(config.db, function(err) {
-    if (err) {
-        console.log('Error establishing database pool. '+err);
-    } else {
-        console.log('Connected to database!');
-    }
+	if (err) {
+		console.log('Error establishing database pool. '+err);
+	} else {
+		console.log('Connected to database!');
+	}
 });
 // var db = mongoose.connect(config.db);
 /**
@@ -25,18 +25,18 @@ var db = mysql.createPool(config.db, function(err) {
  * Please note that the order of loading is important.
  */
 var options = {
-    key: fs.readFileSync(config.sslAssets.key),
-    cert: fs.readFileSync(config.sslAssets.cert)
-    //requestCert: true
+	key: fs.readFileSync(config.sslAssets.key),
+	cert: fs.readFileSync(config.sslAssets.cert)
+	//requestCert: true
 };
 // Bootstrap Models, Dependencies, Routes and the app as an express app
 var app = require('./server/config/system/bootstrap')(passport, db, options);
 
 var httpapp = express(),
-    http = require('http').createServer(httpapp),
-    https = require('https'),
-    server = https.createServer(options, app),
-    io = require('socket.io').listen(server);
+	http = require('http').createServer(httpapp),
+	https = require('https'),
+	server = https.createServer(options, app),
+	io = require('socket.io').listen(server);
 
 require('./server/config/socket.js')(app, passport, io);
 require('./server/config/report.js')(db);
@@ -44,11 +44,11 @@ require('./server/config/report.js')(db);
 var SSLport = process.env.sslPORT || config.SSLport;
 var HTTPport = process.env.httpPORT || config.HTTPport;
 httpapp.get('*',function(req, res){
-    if (config.httpRedirect.link && config.portEnabled) {
-        res.redirect(config.httpRedirect.link+':'+SSLport+req.url);
-    } else {
-        res.redirect(config.httpRedirect.link+'/'+req.url);
-    }
+	if (config.httpRedirect.link && config.httpRedirect.portEnabled) {
+		res.redirect(config.httpRedirect.link+':'+SSLport+req.url);
+	} else {
+		res.redirect(config.httpRedirect.link+req.url);
+	}
 });
 server.listen(SSLport);
 http.listen(HTTPport);
