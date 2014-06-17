0ab84afadaf137ece237ede68fef20c7fcdaf322
Boris Ionine <bionine@phirelight.com>
2014-06-17 16:24:03 -0400

    Last seen health data tables displayed in 2nd level health page in webapp

--- server/controllers/health/health_drill.js
+++ server/controllers/health/health_drill.js
@@ -96,18 +96,119 @@ module.exports = function(pool) {
 						title: 'System Disc Usage'
 					}
 				}
+				var table3 = {
+					query: 'SELECT *, '+
+							'MAX(`timestamp`) '+
+						'FROM ' +
+							'`log2sql_stats` '+
+						'WHERE ' +
+							'`client` = ?',
+					insert: [req.query.client],
+					params: [
+						{
+							title: 'Last Seen',
+							select: 'timestamp',
+							// link: {
+							// 	type: 'http_by_domain_local_drill',
+							// 	// val: the pre-evaluated values from the query above
+							// 	val: ['lan_ip','lan_zone','host'],
+							// 	crumb: false
+							// }
+						},
+						{ title: 'Client', select: 'client' },
+						{ title: 'Connections', select: 'count_conn' },
+						{ title: 'Files', select: 'count_file' },
+						{ title: 'HTTP Connections', select: 'count_http' },
+						{ title: 'DNS Connections', select: 'count_dns' },
+						{ title: 'SSL Connections', select: 'count_ssl' },
+						{ title: 'Unique Remote Connections ', select: 'count_uniq_remote' },
+						{ title: 'Unique Domains', select: 'count_uniq_domain' },
+						{ title: 'Unique Queries', select: 'count_uniq_query' },
+						{ title: 'Unique SSL Connections', select: 'count_uniq_ssl' },
+						{ title: 'IOCs', select: 'ioc_count_total' },
+						{ title: 'SSH Connections', select: 'count_ssh' },
+						{ title: 'IRC Connections', select: 'count_irc' },
+						{ title: 'SMTP Connections', select: 'count_smtp' },
+						{ title: 'FTP Connections', select: 'count_ftp' },
+						{ title: 'Unique SSH Connections', select: 'count_uniq_ssh' },
+						{ title: 'Unique FTP Connections', select: 'count_uniq_ftp' },
+						{ title: 'Conn File', select: 'conn_file' }
+					],
+					settings: {
+						sort: [[1, 'desc']],
+						div: 'table3',
+						title: 'Log2Sql Statistics'
+					}
+				}
+				var table4 = {
+					query: 'SELECT *, '+
+							'MAX(`timestamp`) '+
+						'FROM ' +
+							'`process_health` '+
+						'WHERE ' +
+							'`client` = ? '+
+							'AND `zone` = ? '+
+						'GROUP BY ' +
+							'`process_name`',
+					insert: [req.query.client, req.query.zone],
+					params: [
+						{
+							title: 'Last Seen',
+							select: 'timestamp',
+							// link: {
+							// 	type: 'http_by_domain_local_drill',
+							// 	// val: the pre-evaluated values from the query above
+							// 	val: ['lan_ip','lan_zone','host'],
+							// 	crumb: false
+							// }
+						},
+						{ title: 'Client', select: 'client' },
+						{ title: 'Zone', select: 'zone' },
+						{ title: 'Process Name', select: 'process_name' },
+						{ title: 'Status as Reported by Monit', select: 'status' },
+						{ title: 'PID', select: 'pid' },
+						{ title: 'Parent PID', select: 'parent_pid' },
+						{ title: 'Uptime as Reported by Monit', select: 'uptime' },
+						{ title: 'Memory (kB)', select: 'memory_kB' },
+						{ title: 'Memory (kB) Total', select: 'memory_kB_total' },
+						{ title: 'Memory %', select: 'memory_percent' },
+						{ title: 'Memory % Total', select: 'memory_percent_total' },
+						{ title: 'CPU %', select: 'cpu_percent' },
+						{ title: 'CPU % Total', select: 'cpu_percent_total' },
+						{ title: 'Thread Count', select: 'thread_count' }
+					],
+					settings: {
+						sort: [[1, 'desc']],
+						div: 'table4',
+						title: 'Process Health'
+					}
+				}
 				async.parallel([
 					// Table function(s)
 					function(callback) {
 						new dataTable(table1, {database: 'rp_health', pool: pool}, function(err,data){
-							console.log(table1.query);
-							tables[1] = data;
+							// console.log(table1.query);
+							tables[3] = data;
 							callback();
 						});
 					},
 					function(callback) {
 						new dataTable(table2, {database: 'rp_health', pool: pool}, function(err,data){
-							console.log(table2.query);
+							// console.log(table2.query);
+							tables[2] = data;
+							callback();
+						});
+					},
+					function(callback) {
+						new dataTable(table3, {database: 'rp_health', pool: pool}, function(err,data){
+							// console.log(table3.query);
+							tables[1] = data;
+							callback();
+						});
+					},
+					function(callback) {
+						new dataTable(table4, {database: 'rp_health', pool: pool}, function(err,data){
+							console.log(table4.query);
 							tables[0] = data;
 							callback();
 						});
