ba33a5d3068bcded0b0957c61fce9bc5a9443979
Stefan Amyotte <stefan.amyotte@gmail.com>
2014-09-15 13:39:18 -0400

    page titles

--- public/pages/routes/pages.js
+++ public/pages/routes/pages.js
@@ -448,7 +448,7 @@ angular.module('mean.pages').config(['$stateProvider',
                             loggedin: checkLoggedin
                         },
                         data: {
-                            title: 'Local to Remote Traffic through Stealth',
+                            title: 'Stealth COI Map',
                             daterange: true
                         }
                     })  
@@ -460,7 +460,7 @@ angular.module('mean.pages').config(['$stateProvider',
                             loggedin: checkLoggedin
                         },
                         data: {
-                            title: 'Local to Remote Traffic through Stealth',
+                            title: 'Stealth Secured Connections By User',
                             daterange: true
                         }
                     })      
@@ -550,7 +550,7 @@ angular.module('mean.pages').config(['$stateProvider',
                         })
                         // ENPOINT EVENTS USER DRILL
                             .state('endpoint_events_user_drill', {
-                                url: '/endpoint_events_user_drill?start&end&alert_info&src_user',
+                                url: '/endpoint_events_user_drill?start&end&lan_user&lan_',
                                 templateUrl: 'public/pages/views/general_network/endpoint_events_user_drill.html',
                                 resolve: {
                                     loggedin: checkLoggedin
