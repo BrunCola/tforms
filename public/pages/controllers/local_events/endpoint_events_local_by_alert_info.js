aa3c79da9f3b6a123a79c5d985005140da5c05ad
Stefan Amyotte <stefan.amyotte@gmail.com>
2014-09-16 07:21:57 -0400

    public local_events

---
+++ public/pages/controllers/local_events/endpoint_events_local_by_alert_info.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('mean.pages').controller('endpointEventsLocalByAlertInfoController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', function ($scope, $stateParams, $location, Global, $rootScope, $http) {
+	$scope.global = Global;
+	var query;
+	if ($location.$$search.start && $location.$$search.end) {
+		query = '/general_network/endpoint_events_local_by_alert_info?start='+$location.$$search.start+'&end='+$location.$$search.end+'&lan_zone='+$location.$$search.lan_zone+'&lan_user='+$location.$$search.lan_user+'&lan_ip='+$location.$$search.lan_ip;
+	} else {
+		query = '/general_network/endpoint_events_local_by_alert_info?&lan_zone='+$location.$$search.lan_zone+'&lan_user='+$location.$$search.lan_user+'&lan_ip='+$location.$$search.lan_ip;
+	}
+	$http({method: 'GET', url: query}).
+	//success(function(data, status, headers, config) {
+	success(function(data) {
+		if (data.tables[0] === null) {
+			$scope.$broadcast('loadError');
+		} else {
+			var dateFormat = d3.time.format('%Y-%m-%d %H:%M:%S');
+
+			$scope.data = data;
+
+			$scope.tableCrossfitler = crossfilter($scope.data.tables[0].aaData);
+			$scope.tableData = $scope.tableCrossfitler.dimension(function(d){return d;});
+			$scope.$broadcast('tableLoad', $scope.tableData, $scope.data.tables, null);
+			$scope.$broadcast('spinnerHide');
+
+		}
+	});
+}]);
