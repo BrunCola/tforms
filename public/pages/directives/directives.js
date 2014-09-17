91ab5ee1be854d24cb05c36e9646a3404baf9008
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-09-17 14:24:32 -0400

    progress to page2

--- public/pages/directives/directives.js
+++ public/pages/directives/directives.js
@@ -2745,37 +2745,35 @@ angular.module('mean.pages').directive('laneGraph', ['$timeout', '$location', 'a
                                         $('#'+previousID).attr('class', null);
 
                                         // console.log(d.id)
-                                        // var thisNode = itemRects.select('g.'+d.id);
+                                        // // var thisNode = itemRects.select('g.'+d.id);
                                          
-                                        itemRects.selectAll('g').each(function(c){
-                                            var elm = d3.select(this);
-                                            if (c.id === d.id) {
-                                                previousElm.attr('class', null);
-                                                elm.attr('class', 'pointactive');
-                                                previousElm = elm;
-                                            }        
-                                        })
+                                        // itemRects.selectAll('g').each(function(c){
+                                        //     var elm = d3.select(this);
+                                        //     if (c.id === d.id) {
+                                        //         previousElm.attr('class', null);
+                                        //         elm.attr('class', 'pointactive');
+                                        //         previousElm = elm;
+                                        //     }        
+                                        // })
 
+                                        // console.log(thisNode)
+                                        // // todo: select the current node somehow so i can apply style to it and creat e previousNode refrence
+                                        // // get this id
 
-
-                                        console.log(thisNode)
-                                        // todo: select the current node somehow so i can apply style to it and creat e previousNode refrence
-
-                                        // get this id
                                         var row = d3.select(this);
                                         var id = row.attr('id'); 
                                             row.attr('class', 'laneactive');
-                                        // // iterate through points
-                                        // itemRects.selectAll('g').each(function(d){
-                                        //     var elm = d3.select(this);
-                                        //     // if id's (of just clicked) match
-                                        //     if (d.id.toString() === id.toString()) {
-                                        //         elm.attr('class', 'pointactive');
-                                        //         previousElm = d3.select(this);
-                                        //     } else {
-                                        //         elm.attr('class', null);
-                                        //     }
-                                        // })
+                                        // iterate through points
+                                        itemRects.selectAll('g').each(function(d){
+                                            var elm = d3.select(this);
+                                            // if id's (of just clicked) match
+                                            if (d.id.toString() === id.toString()) {
+                                                elm.attr('class', 'pointactive');
+                                                previousElm = elm;
+                                            } else {
+                                                elm.attr('class', null);
+                                            }
+                                        })
 
                                         // set previous id
                                         previousID = id;
