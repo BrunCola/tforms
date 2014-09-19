701b1cf7631a1dfea2a315c22ec09279c3b5cf14
Stefan Amyotte <stefan.amyotte@gmail.com>
2014-09-19 15:26:40 -0400

    ioc and stealth query

--- server/controllers/constructors/lanegraph.js
+++ server/controllers/constructors/lanegraph.js
@@ -57,7 +57,7 @@ module.exports = function (sql, conn, callback) {
                 if (index === null) {
                     index = conn.lanes.indexOf(data.type);
                     if (data.type.search('ioc') !== -1) {
-                        index = conn.lanes.indexOf('IOC');
+                        index = conn.lanes.indexOf('IOC'`);
                     }
                     if (data.type.search('Stealth') !== -1) {
                         index = conn.lanes.indexOf('Stealth');
