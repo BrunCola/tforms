deac4c15a8b3e74cb12fafde1e740b028d8638c9
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-09-22 14:39:04 -0400

    coi visual changes

--- public/pages/controllers/local_events/local_COI_remote.js
+++ public/pages/controllers/local_events/local_COI_remote.js
@@ -21,14 +21,53 @@ angular.module('mean.pages').controller('localCoiRemoteController', ['$scope', '
     });
 
     $scope.requery = function(data, button) {
-        console.log(data)
-        var results = $scope.forcedata.links.filter(function(d){
-            if ((d.class !== undefined) && ((d.source.index === data.index) || (d.target.index === data.index))) {
-                return d.target;
-            }
-        })
-        console.log(results)
-        console.log($scope.forcedata)
-        $scope.appendInfo({'test': 'test'})
+        var results = [];
+        switch(button) {
+            case 'blocked':
+            // get children hanging off of parent nodes
+                var rTargets = $scope.forcedata.links.filter(function(d){
+                    if ((d.class !== undefined) && (d.source.index === data.index)) {
+                        return true;
+                    }
+                })
+                var rSource = $scope.forcedata.links.filter(function(d){
+                    if ((d.class !== undefined) && (d.target.index === data.index)) {
+                        return true;
+                    }
+                })
+                for (var i in rTargets) {
+                    results.push($scope.forcedata.nodes[rTargets[i].target.index].value)
+                }
+                for (var i in rSource) {
+                    results.push($scope.forcedata.nodes[rSource[i].source.index].value)
+                }
+            break;
+            case 'users':
+                var thisObj = $scope.forcedata.uniqueNodes[data.name];
+                for (var i in thisObj) {
+                    var obj = {};
+                    obj[i] = Object.keys($scope.forcedata.uniqueUsers[i]);
+                    results.push(obj)
+                }
+            break;
+            case 'top':
+                // clear the div (replace with loading thing later)
+                $scope.appendInfo({});
+                query += '&type=top'
+                $http({method: 'GET', url: query}).
+                //success(function(data, status, headers, config) {
+                success(function(data) {
+                    if (data.force === null) {
+                        $scope.$broadcast('loadError');
+                    } else {
+                        // $scope.forcedata = data.force;
+                        // $scope.$broadcast('forceChart', data.force, {height: 1000});
+                        // $scope.$broadcast('spinnerHide');
+                        console.log(data)
+                    }
+                });
+            break;
+        }
+        $scope.appendInfo(results);
     }
 }]);
