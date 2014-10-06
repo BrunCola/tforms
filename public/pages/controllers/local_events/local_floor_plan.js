44eb961ffc140e38f8767d6960ba2fec2364c43c
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-10-06 09:38:23 -0400

    progress

--- public/pages/controllers/local_events/local_floor_plan.js
+++ public/pages/controllers/local_events/local_floor_plan.js
@@ -2,8 +2,7 @@
 
 angular.module('mean.pages').controller('floorPlanController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', '$modal', function ($scope, $stateParams, $location, Global, $rootScope, $http, $modal) {
     $scope.global = Global;
-    var query;
-    query = '/local_events/local_floor_plan?';
+    var query = '/local_events/local_floor_plan?';
     $http({method: 'GET', url: query}).
     //success(function(data, status, headers, config) {
     success(function(data) {
@@ -11,7 +10,11 @@ angular.module('mean.pages').controller('floorPlanController', ['$scope', '$stat
             $scope.$broadcast('loadError');
         } else {
             $scope.data = data;
-            $scope.$broadcast('floorPlan', data.force, {height: 1000});
+            var count = 0;
+            $scope.data.force.forEach(function(d){
+                d.id = count++;
+            })
+            $scope.$broadcast('floorPlan', $scope.data.force);
             $scope.$broadcast('spinnerHide');
         }
     });  
