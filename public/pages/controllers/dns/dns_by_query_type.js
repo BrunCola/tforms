6b085c2e8bb849937247d68e461907962cd77d4a
Boris Ionine <bionine@phirelight.com>
2014-10-28 10:49:11 -0400

    barchart for DNS

--- public/pages/controllers/dns/dns_by_query_type.js
+++ public/pages/controllers/dns/dns_by_query_type.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean.pages').controller('dnsByQueryTypeController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', function ($scope, $stateParams, $location, Global, $rootScope, $http) {
+angular.module('mean.pages').controller('dnsByQueryTypeController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', 'timeFormat', function ($scope, $stateParams, $location, Global, $rootScope, $http, timeFormat) {
     $scope.global = Global;
     var query;
     if ($location.$$search.start && $location.$$search.end) {
@@ -14,11 +14,24 @@ angular.module('mean.pages').controller('dnsByQueryTypeController', ['$scope', '
         if (data.tables[0] === null) {
             $scope.$broadcast('loadError');
         } else {
+            data.crossfilter.forEach(function(d) {
+                d.dd = timeFormat(d.time, 'strdDateObj');
+                d.hour = d3.time.hour(d.dd);
+                d.count = +d.count;
+            });
+            $scope.crossfilterData = crossfilter(data.crossfilter);
             $scope.data = data;
+
             $scope.tableCrossfitler = crossfilter($scope.data.tables[0].aaData);
             $scope.tableData = $scope.tableCrossfitler.dimension(function(d){return d;});
             $scope.$broadcast('tableLoad', $scope.tableData, $scope.data.tables, null);
             $scope.$broadcast('spinnerHide');
+
+            var barDimension = $scope.crossfilterData.dimension(function(d) { return d.hour });
+            var barGroup = barDimension.group().reduceSum(function(d) { return d.count });
+            $scope.$broadcast('barChart', barDimension, barGroup, 'bar');
+                $scope.barChartxAxis = '';
+                $scope.barChartyAxis = '# DNS Queries / Hour';
         }
     });
 }]);
--- server/controllers/dns/dns_by_query_type.js
+++ server/controllers/dns/dns_by_query_type.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var dataTable = require('../constructors/datatable'),
+    query = require('../constructors/query'),
     config = require('../../config/config'),
     async = require('async');
 
@@ -15,6 +16,7 @@ module.exports = function(pool) {
                 end = req.query.end;
             }
             var tables = [];
+            var crossfilter = [];
             var info = [];
             var table1 = {
                 query: 'SELECT '+
@@ -54,6 +56,23 @@ module.exports = function(pool) {
                     access: req.session.passport.user.level
                 }
             }
+            var crossfilterQ = {
+                query: 'SELECT '+
+                        'count(*) AS count,'+
+                        'time,'+
+                        '`qtype`,'+
+                        '`qtype_name` '+
+                    'FROM '+
+                        '`dns` '+
+                    'WHERE '+
+                        '`time` BETWEEN ? AND ? '+
+                    'GROUP BY '+
+                        'month(from_unixtime(`time`)),'+
+                        'day(from_unixtime(`time`)),'+
+                        'hour(from_unixtime(`time`)),'+
+                        '`qtype`',
+                insert: [start, end]
+            }
             async.parallel([
                 // Table function(s)
                 function(callback) {
@@ -62,11 +81,19 @@ module.exports = function(pool) {
                         callback();
                     });
                 },
+                // Crossfilter function
+                function(callback) {
+                    new query(crossfilterQ, {database: database, pool: pool}, function(err,data){
+                        crossfilter = data;
+                        callback();
+                    });
+                }
             ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
                 if (err) throw console.log(err);
                 var results = {
                     info: info,
-                    tables: tables
+                    tables: tables,
+                    crossfilter: crossfilter
                 };
                 res.json(results);
             });
