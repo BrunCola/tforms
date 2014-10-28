f522bd8e4cc0ecf1bfec0bfa2e33ccda6e6a0df2
Boris Ionine <bionine@phirelight.com>
2014-10-28 12:27:15 -0400

    visuals for HTTP by User Agent

--- public/pages/controllers/http/http_by_user_agent.js
+++ public/pages/controllers/http/http_by_user_agent.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean.pages').controller('httpByUserAgentController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', function ($scope, $stateParams, $location, Global, $rootScope, $http) {
+angular.module('mean.pages').controller('httpByUserAgentController', ['$scope', '$stateParams', '$location', 'Global', '$rootScope', '$http', 'timeFormat', function ($scope, $stateParams, $location, Global, $rootScope, $http, timeFormat) {
     $scope.global = Global;
     var query;
     if ($location.$$search.start && $location.$$search.end) {
@@ -14,11 +14,59 @@ angular.module('mean.pages').controller('httpByUserAgentController', ['$scope', 
         if (data.tables[0] === null) {
             $scope.$broadcast('loadError');
         } else {
+            data.crossfilter.forEach(function(d) {
+                d.dd = timeFormat(d.time, 'strdDateObj');
+                d.hour = d3.time.hour(d.dd);
+                d.in_bytes = parseInt(d.in_bytes);
+                d.count = +d.count;
+            });
+            $scope.crossfilterData = crossfilter(data.crossfilter);
+            $scope.piechartData = crossfilter(data.piechart);
             $scope.data = data;
+
             $scope.tableCrossfitler = crossfilter($scope.data.tables[0].aaData);
             $scope.tableData = $scope.tableCrossfitler.dimension(function(d){return d;});
             $scope.$broadcast('tableLoad', $scope.tableData, $scope.data.tables, null);
-            $scope.$broadcast('spinnerHide');
+            // $scope.$broadcast('spinnerHide');
+
+            var barDimension = $scope.crossfilterData.dimension(function(d) { return d.hour });
+            var barGroupPre = barDimension.group();
+            var barGroup = barGroupPre.reduce(
+                function(p, v) {
+                    p.in_bytes += v.in_bytes;
+                    p.out_bytes += v.out_bytes;
+                    return p;
+                },
+                function(p, v) {
+                    p.in_bytes -= v.in_bytes;
+                    p.out_bytes -= v.out_bytes;
+                    return p;
+                },
+                function() {
+                    return {
+                        in_bytes: 0,
+                        out_bytes: 0
+                    };
+                }
+            );
+            $scope.$broadcast('barChart', barDimension, barGroup, 'bandwidth');
+
+            var countDimension = $scope.piechartData.dimension(function(d) { return d.count }).top(10).map(function(d){ return d.pie_dimension });
+            $scope.appDimension = $scope.piechartData.dimension(function(d) { 
+                if(countDimension.indexOf(d.pie_dimension) !== -1) {
+                    return d.pie_dimension;
+                } else {
+                    return "Other";
+                }
+            });                 
+            $scope.pieGroup = $scope.appDimension.group().reduceSum(function (d) {
+                return d.count;
+            });
+            // console.log(pieGroup.top(Infinity));
+            $scope.$broadcast('pieChart', 'application');
+
+            $scope.barChartxAxis = '';
+            $scope.barChartyAxis = '# MB / Hour';
         }
     });
 }]);
