58da01786c94349a6d100d21640fe10c9e425791
Boris Ionine <bionine@phirelight.com>
2014-11-10 12:45:57 -0500

    auto refresh for IOC by Events...should be working, but not 100% tested. Refresh set to 30 seconds

--- public/pages/controllers/ioc_notifications/ioc_events.js
+++ public/pages/controllers/ioc_notifications/ioc_events.js
@@ -15,15 +15,118 @@ angular.module('mean.pages').controller('iocEventsController', ['$scope', '$stat
         if (data.tables[0] === null) {
             $scope.$broadcast('loadError');
         } else {
-            data.crossfilter.forEach(function(d) {
-                d.dd = timeFormat(d.time, 'strdDateObj');
-                d.hour = d3.time.hour(d.dd);
-                d.count = +d.count;
-            });
+            processData(data, false);            
+        }
+    });
+    //auto refresh using angular interval
+    var refreshPeriod = 30000; //in milliseconds (30 seconds)
+    var promise = $interval(function() {
+        console.log("AUTO REFRESH");
+        var newStart, newEnd;
+        if ($location.$$search.start && $location.$$search.end) {
+            newEnd = parseInt($location.$$search.end) + refreshPeriod / 1000;
+            newStart = $location.$$search.end;            
+
+            query = '/ioc_notifications/ioc_events?start='+newStart+'&end='+newEnd;
+
+            $location.$$search.end = "" + (parseInt($location.$$search.end) + refreshPeriod / 1000);
+        } else {
+            newEnd = new Date().getTime() / 1000; 
+            newStart = newEnd - refreshPeriod / 1000;
+
+            query = '/ioc_notifications/ioc_events?start='+newStart+'&end='+newEnd;
+        }
+        $http({method: 'GET', url: query}).
+        success(function(data) {
+            console.log(data);
+            //TODO: Add the new data to all the crossfilters and broadcast to the directives.
+            //Filter out the timeslice between the old start and old start + refresh period (and update directives)
+
+            processData(data, true);
+
+
+        });
+    }, refreshPeriod);
+
+        function processData(data, autoRefresh) {
+        data.crossfilter.forEach(function(d) {
+            d.dd = timeFormat(d.time, 'strdDateObj');
+            d.hour = d3.time.hour(d.dd);
+            d.count = +d.count;
+        }); 
+
+        var newData = false;
+        //if this is a call from an auto refresh, just add the data to the existing crossfilters
+        //otherwise create the crossfilters
+        if(autoRefresh) {
+            newData = (data.crossfilter.length > 0) && (data.tables[0] != null);
+            if(newData) {
+                $scope.crossfilterData.add(data.crossfilter);
+                $scope.tableCrossfitler.add(data.tables[0].aaData);
+
+                $scope.data.add(data);//??
+            } 
+            // else {//TEMPORARY FOR TESTING
+            //     data.crossfilter = [{
+            //         count: 1, 
+            //         dd: "Mon Nov 10 2014 09:21:47 GMT+0000 (GMT)",
+            //         hour: "Mon Nov 10 2014 09:00:00 GMT+0000 (GMT)",
+            //         in_bytes: 0.0018,
+            //         ioc: "Known Hostile IP",
+            //         ioc_severity: 3,
+            //         out_bytes: 0.0022,
+            //         remote_country: "United States",
+            //         time: 1415640420,
+            //     }];
+            //     data.crossfilter.forEach(function(d) {
+            //         d.dd = timeFormat(d.time, 'strdDateObj');
+            //         d.hour = d3.time.hour(d.dd);
+            //         d.count = +d.count;
+            //     });
+
+            //     data.tables[.push({
+            //         aaData: [{
+            //         in_bytes: 44,
+            //         in_packets: 1,
+            //         ioc: "Suspected Hostile IP",
+            //         ioc_attrID: "200494",
+            //         ioc_childID: "200494",
+            //         ioc_count: 1,
+            //         ioc_rule: "141.212.121.0/24",
+            //         ioc_severity: 2,
+            //         ioc_typeIndicator: "IP Subnet",
+            //         ioc_typeInfection: "Pre-Infection",
+            //         lan_ip: "10.0.0.30",
+            //         lan_user: "-",
+            //         lan_zone: "Dev",
+            //         machine: "-",
+            //         out_bytes: 84,
+            //         out_packets: 2,
+            //         proxy_blocked: 0,
+            //         remote_asn_name: "UMICH-AS-5",
+            //         remote_cc: "US",
+            //         remote_country: "United States",
+            //         remote_ip: "141.212.121.61",
+            //         stealth: 0,
+            //         time: 1415640420.396433}]
+            //     });
+            //     console.log(data.tables);
+            //     newData = true;
+            //     $scope.crossfilterData.add(data.crossfilter);
+            //     $scope.tableCrossfitler.add(data.tables[0].aaData);
+
+            //     $scope.data.add(data);//??
+            // }
+
+        } else {
+            newData = true;
             $scope.crossfilterData = crossfilter(data.crossfilter);
             $scope.data = data;
-
+            console.log($scope.data.tables[0]);
             $scope.tableCrossfitler = crossfilter($scope.data.tables[0].aaData);
+        }
+
+        if(newData) {
             $scope.tableData = $scope.tableCrossfitler.dimension(function(d){return d;});
             $scope.$broadcast('tableLoad', $scope.tableData, $scope.data.tables, null);
 
@@ -114,36 +217,8 @@ angular.module('mean.pages').controller('iocEventsController', ['$scope', '$stat
             $scope.barChartxAxis = '';
             $scope.barChartyAxis = '# IOC / Hour';
             $scope.$broadcast('severityLoad');
-
-            
         }
-    });
-
-    //auto refresh using angular interval
-    var refreshPeriod = 10000; //in milliseconds (10 seconds)
-    var promise = $interval(function() {
-        console.log("AUTO REFRESH");
-        var newStart, newEnd;
-        if ($location.$$search.start && $location.$$search.end) {
-            newEnd = parseInt($location.$$search.end) + refreshPeriod / 1000;
-            newStart = $location.$$search.end;            
-
-            query = '/ioc_notifications/ioc_events?start='+newStart+'&end='+newEnd;
-
-            $location.$$search.end = "" + (parseInt($location.$$search.end) + refreshPeriod / 1000);
-        } else {
-            newEnd = new Date().getTime() / 1000; 
-            newStart = newEnd - refreshPeriod / 1000;
-
-            query = '/ioc_notifications/ioc_events?start='+newStart+'&end='+newEnd;
-        }
-        $http({method: 'GET', url: query}).
-        success(function(data) {
-            console.log(data);
-            //TODO: Add the new data to all the crossfilters and broadcast to the directives.
-            //Filter out the timeslice between the old start and old start + refresh period (and update directives)
-        });
-    }, refreshPeriod);
+    }
 
     $http({method: 'GET', url: query+'&type=ioc_notifications'}).
     success(function(data) {
