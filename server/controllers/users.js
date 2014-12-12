69973ac920da5832a6f3f08e0b1526e9509a91e7
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2014-12-12 14:53:21 -0500

    added log in console log

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,6 +8,8 @@ module.exports = function(pool) {
     return {
         login: function(req, res) {
             pool.query("SELECT * FROM user WHERE email = ? limit 1", [req.body.email], function(err, data){
+                var time = new Date();
+                console.log('Email/login: '+data[0].email+', Database:'+data[0].database+', Time: '+time)
                 if (data.length !== 1) { res.send(401, 'Wrong user or password'); return; }
                 var profile = data[0];
                 bcrypt.compare(req.body.password, profile.password, function(err, doesMatch){
@@ -23,8 +25,8 @@ module.exports = function(pool) {
             });
         },
         loggedin: function(req, res) {
-            console.log(req.user);
-            console.log('LIMIT THIS RETURN')
+            // console.log(req.user);
+            // console.log('LIMIT THIS RETURN')
             if (req.user) {
                 res.send(200, req.user)
             } else {
