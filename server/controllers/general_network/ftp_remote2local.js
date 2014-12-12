161cb7b240688dda85e490bcada55792337f721d
Stefan Amyotte <stefan.amyotte@gmail.com>
2014-12-12 07:36:09 -0500

    ftp cleanup

--- server/controllers/general_network/ftp_remote2local.js
+++ server/controllers/general_network/ftp_remote2local.js
@@ -20,37 +20,38 @@ module.exports = function(pool) {
                 var info = [];
                 var table1 = {
                     query: 'SELECT '+
-                                'count(*) AS count,'+
-                                'max(`time`) as `time`,'+
+                                'count(*) AS count,' +
+                                'max(`time`) AS `time`,'+
                                 '`lan_stealth`,'+
-                                '`lan_zone`,'+
                                 '`lan_machine`,'+
+                                '`lan_zone`,'+
                                 '`lan_user`,'+
-                                '`lan_ip,'+
+                                '`lan_ip`,'+
                                 '`lan_port`,'+
-                                '`remote_ip`,'+
-                                '`remote_port`,'+
-                                '`remote_cc`,'+
-                                '`remote_country`,'+
-                                '`remote_asn_name`,'+
-                                '`user` AS `ftp_user`,'+
-                                '`password`,'+
-                                '`command`,'+
-                                '`arg`,'+
-                                '`mime_type`,'+
-                                '`file_size`,'+
-                                '`reply_code`,'+
-                                '`reply_msg`,'+
-                                '`dc_passive`,'+
-                                '`dc_orig_h`,'+
-                                '`dc_resp_h`,'+
-                                '`dc_resp_p`,'+
-                                '`ioc`,'+
-                                '`ioc_severity`,'+
-                                '`ioc_typeInfection`,'+
-                                '`ioc_typeIndicator`,'+
-                                '`ioc_rule`,'+
-                                'sum(`ioc_count`) AS `ioc_count` '+
+                                '`remote_ip`, ' +
+                                '`remote_port`, '  +
+                                '`remote_cc`, ' +
+                                '`remote_country`, ' +
+                                '`remote_asn`, ' +
+                                '`remote_asn_name`, ' +
+                                '`user`, ' +
+                                '`password`, ' +
+                                '`command`, ' +
+                                '`arg`, ' +
+                                '`mime_type`, ' +
+                                '`file_size`, ' +
+                                '`reply_code`, ' +
+                                '`reply_msg`, ' +
+                                '`dc_passive`, ' +
+                                '`dc_orig_h`, ' +
+                                '`dc_resp_h`, ' +
+                                '`dc_resp_p`, ' +
+                                '`ioc`, ' +
+                                '`ioc_severity`, ' +
+                                '`ioc_typeInfection`, ' +
+                                '`ioc_typeIndicator`, ' +
+                                '`ioc_rule`, ' +
+                                'sum(`ioc_count`) AS `ioc_count` ' +
                             'FROM ' +
                                 '`ftp` '+
                             'WHERE '+ 
@@ -81,7 +82,7 @@ module.exports = function(pool) {
                         { title: 'Flag', select: 'remote_cc' },
                         { title: 'Remote Country', select: 'remote_country' },
                         { title: 'Remote ASN Name', select: 'remote_asn_name' },
-                        { title: 'User', select: 'ftp_user' },
+                        { title: 'User', select: 'user' },
                         { title: 'Password', select: 'password' },
                         { title: 'Command', select: 'command' },
                         { title: 'Arg', select: 'arg' },
@@ -94,10 +95,10 @@ module.exports = function(pool) {
                         { title: 'DC Resp H', select: 'dc_resp_h' },
                         { title: 'DC Resp P', select: 'dc_resp_p' },
                         { title: 'IOC', select: 'ioc' },
-                        { title: 'IOC Severity', select: 'ioc_severity' },
-                        { title: 'Infection Stage', select: 'ioc_typeInfection' },
-                        { title: 'Indicator Type', select: 'ioc_typeIndicator' },
+                        { title: 'IOC Type', select: 'ioc_typeIndicator' },
+                        { title: 'IOC Stage', select: 'ioc_typeInfection' },
                         { title: 'IOC Rule', select: 'ioc_rule' },
+                        { title: 'IOC Severity', select: 'ioc_severity' },
                         { title: 'IOC Count', select: 'ioc_count' }
                     ],
                     settings: {
