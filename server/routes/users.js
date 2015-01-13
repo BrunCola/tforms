5efe0c847350d92271165ca8f7418ae61285dbc1
Andrew Dillion-Walshe <andrewdillion6@Andrews-MacBook-Pro.local>
2015-01-12 19:26:29 -0500

    almost done fixing this

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,21 +1,30 @@
 'use strict';
 
 // User routes use users controller
-var users = require('../controllers/users');
+var users = require('../controllers/users'),
+    auth = require('./middlewares/authorization')();
 
 module.exports = function (app, version, pool) {
 
-	// User routes use users controller
-	var users = require('../controllers/users')(pool);
-	
-	app.route('/api/loggedin')
-		.get(users.loggedin)
-		
-	// Setting the local strategy route
-	app.route('/auth')
-		.post(users.login);
+    // User routes use users controller
+    var users = require('../controllers/users')(pool);
+    
+    app.route('/api/loggedin')
+        .get(users.loggedin)
+        
+    // Setting the local strategy route
+    app.route('/auth')
+        .post(users.login);
 
-	app.route('/2factor/verify')
-		.post(users.twoStep);
+    // 2auth verify route
+    app.route('/2factor/verify')
+        .post(users.twoStep);
+
+    app.route('/api/users/updatepass')
+        .post(auth.permission, users.updatePassword);
+
+    app.route('/api/users/updateemail')
+        .post(auth.permission, users.updateEmail);
+
 
 };
