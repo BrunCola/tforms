f44eb5a23482ce3f7461fd1326c9bc28354183f0
Bruno <bcolantonio@phirelight.com>
2015-01-15 17:24:50 -0500

    removed local ping live conns

--- public/pages/directives/map.js
+++ public/pages/directives/map.js
@@ -302,14 +302,16 @@ angular.module('mean.pages').directive('makeMap', ['$timeout', '$location', '$ro
                                 }
                             });
                     }
-                    parentt.append("circle")
-                        .attr("transform", function(d) {return "translate(" + projection([d.zone.coordinates[0],d.zone.coordinates[1]]) + ")";})
-                        .attr("r", 2)
-                        .attr("class",  "center")
-                        .style("stroke", function(d) {
-                            //return cc[d.properties.lan_zone];
-                            return "#f0f";
-                        })
+                    //       -------------------------------------------------- local ping
+                    // parentt.append("circle")
+                    //     .attr("transform", function(d) {return "translate(" + projection([d.zone.coordinates[0],d.zone.coordinates[1]]) + ")";})
+                    //     .attr("r", 2)
+                    //     .attr("class",  "center")
+                    //     .style("stroke", function(d) {
+                    //         //return cc[d.properties.lan_zone];
+                    //         return "#f0f";
+                    //     })
+                    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   local ping
                 });
                 /*show node info on mouseover*/
                 var tip = d3.tip().attr('class', 'd3-tip')
@@ -477,25 +479,28 @@ angular.module('mean.pages').directive('makeMap', ['$timeout', '$location', '$ro
                         })
                         .style("fill-opacity", 1e-6)
                         .remove();
-                     point
-                            .append("circle")
-                            .attr("r", 2)
-                            .style("fill-opacity", 0.9)
-                            .attr("transform", function(d) {return "translate(" + projection([d.zone.coordinates[0],d.zone.coordinates[1]]) + ")";})
-                            .transition()
-                            .duration(function(d) {
-                                return 800 / (d.properties.count/5);
-                            })
-                            .ease(Math.sqrt)
-                            .attr("r", function(d) {
-                                return d.properties.count*50;
-                            })
-                            .style('fill', function(d){
-                                //return cc[d.properties.lan_zone];
-                                return "#f0f";
-                            })
-                            .style("fill-opacity", 1e-6)
-                            .remove();
+
+                     //       -------------------------------------------------- local ping
+                     // point
+                     //        .append("circle")
+                     //        .attr("r", 2)
+                     //        .style("fill-opacity", 0.9)
+                     //        .attr("transform", function(d) {return "translate(" + projection([d.zone.coordinates[0],d.zone.coordinates[1]]) + ")";})
+                     //        .transition()
+                     //        .duration(function(d) {
+                     //            return 800 / (d.properties.count/5);
+                     //        })
+                     //        .ease(Math.sqrt)
+                     //        .attr("r", function(d) {
+                     //            return d.properties.count*50;
+                     //        })
+                     //        .style('fill', function(d){
+                     //            //return cc[d.properties.lan_zone];
+                     //            return "#f0f";
+                     //        })
+                     //        .style("fill-opacity", 1e-6)
+                     //        .remove();
+                     //-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ local ping
                     point
                         .transition()
                         .duration(function(d){
